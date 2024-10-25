c32687420fc152506579b7fc622176a0601ea774
Muktadir-SoftWorks <muktadirr.info@gmail.com>
2024-10-25 22:58:35 +0600

    Practice package

---
+++ main.py
@@ -0,0 +1,15 @@
+# import math_operations
+# print(math_operations.multiply(20,3))
+
+# from math_operations import add,demo,multiply
+#
+# print(add(90,20))
+# print(multiply(6,3))
+#
+#
+# obj = demo()
+# print(obj.sub(20,4))
+
+
+from My_package.module_1 import calculate
+print(calculate(20))
---
+++ math_operations.py
@@ -0,0 +1,16 @@
+def add(a,b):
+    return a+b
+
+def multiply(a,b):
+    return a*b
+
+a = 20
+b = 30
+
+class demo:
+
+    def __init__(self):
+        print("I am class constructor")
+
+    def sub(self,a,b):
+        return a-b
